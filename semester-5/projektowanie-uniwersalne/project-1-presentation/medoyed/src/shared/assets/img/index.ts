41e1a89b2c740518daae20537975720b71bedff8
MEDOYED <mmax26072005@gmail.com>
2025-11-04 09:28:22 +0100

    fix(projektowanie-uniwersalne/project-1): add barrel-exports with aliases paths

---
+++ semester-5/projektowanie-uniwersalne/project-1-presentation/medoyed/src/shared/assets/img/index.ts
@@ -0,0 +1,1 @@
+export { default as HobbyImg } from "./hobby-img.png";
---
+++ semester-5/projektowanie-uniwersalne/project-1-presentation/medoyed/src/shared/types/images.d.ts
@@ -0,0 +1,20 @@
+// .d --> declaration file
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
